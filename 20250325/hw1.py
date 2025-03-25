2a689c6c362a117519ac24a58053c4f05b59eef4
張力仁 <jacky1126@gmail.com>
2025-03-25 18:50:53 +0800

    Create hw1.py

---
+++ 20250325/hw1.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_excel('311.xlsx')
+
+df['sum']=df['x']+df['y']
+
+print(df['sum'])
+
+plt.scatter(df['x'],df['y'])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Scatter plot of x and y')
+plt.show()
